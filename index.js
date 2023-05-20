42430a995735a92475c23e109e0d212a78af4c2a
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-20 17:15:40 +0100

    Install Commander and get API key

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import { Configuration, OpenAIApi } from "openai";
+import { program } from 'commander';
+
+program
+  .option('-k, --key <type>');
+
+program.parse();
+
+const options = program.opts();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: options.key,
 });
 
 const openai = new OpenAIApi(configuration);
