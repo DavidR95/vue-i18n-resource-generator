23233edcab4a9356b454f7a07a220c07d1252722
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-20 17:32:57 +0100

    Generate translations from input file

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import { Configuration, OpenAIApi } from "openai";
 import { program } from 'commander';
+import fs from 'fs/promises';
 
 program
-  .option('-k, --key <type>');
+  .option('-k, --key <type>')
+  .option('-i, --input-path <type>');
 
 program.parse();
 
 const options = program.opts();
+
+let data = '';
+
+try {
+    data = await fs.readFile(options.inputPath, { encoding: 'utf8' });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 
 const configuration = new Configuration({
   apiKey: options.key,
@@ -16,8 +27,10 @@ const openai = new OpenAIApi(configuration);
 
 const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: 'say hello world',
+    prompt: `Translate the values in the following JSON in to the following locales: 'de-DE'. Translation must adhere to the Vue I18n message syntax.
+    ${data}`,
     temperature: 0,
+    max_tokens: 100,
   });
 
 console.log(completion.data.choices[0].text);
