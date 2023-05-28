ef20bdf52b881b475b9b5c58b742980525703b91
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 16:32:17 +0100

    Get name, descrioption and version from package JSON

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { Command } from 'commander';
+import { name, description, version } from '../package.json';
 
 const program = new Command();
-console.log('hello world');
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .name(name)
+  .description(description)
+  .version(version);
 
 program.option('-k, --key <type>').option('-i, --input-path <type>');
 // .option('-l, --locales <type>', 'hm', commaSeparatedList);
 
 program.parse();
 
-console.log(program.opts())
+console.log(program.opts());
