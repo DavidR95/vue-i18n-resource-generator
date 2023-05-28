0e2bae9c51908d237fdf988a89b85267177627a9
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 16:53:42 +0100

    Add all necessary command options

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import { name, description, version } from '../package.json';
 
 const program = new Command();
 
-program
-  .name(name)
-  .description(description)
-  .version(version);
+program.name(name).description(description).version(version)
 
-program.option('-k, --key <type>').option('-i, --input-path <type>');
-// .option('-l, --locales <type>', 'hm', commaSeparatedList);
+program
+  .requiredOption('-k, --key <key>', 'your OpenAI API key')
+  .requiredOption(
+    '-i, --input-path <path>',
+    'the path to the JSON file containing the Vue I18n messages you would like to translate',
+  )
+  .option(
+    '-o, --output-path <path>',
+    'the path to the directory where you would like your translated JSON files to be written to',
+    process.cwd()
+  )
+  .requiredOption('-l, --locales <locales...>', 'the list of locales you would like to translate your input messages in to');
 
 program.parse();
 
