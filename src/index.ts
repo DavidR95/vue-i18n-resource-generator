ba0fa662ab9f75e8924e899f0ad650e8d4845cee
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 17:33:04 +0100

    Add initial argument logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { createCommand } from 'commander';
 import chalk from 'chalk';
 import { name, description, version } from '../package.json';
 
+const NUMBER_OF_API_KEY_CHARACTERS_TO_SHOW = 4;
+
+const { log } = console;
+
 const command = createCommand();
 
 command
@@ -29,9 +33,18 @@ command
 
 command.parse();
 
-const { log } = console;
-
 const commandOptions = command.opts();
 
-log(chalk.blue(`Using API key ending in ${chalk.bold.italic(commandOptions['key'].slice(-4))}`));
+const { key, inputPath, locales } = commandOptions;
 
+log(chalk.magenta(`Attempting to translate Vue I18n messages found within the JSON file located at: ${chalk.bold.underline(inputPath)} \n`));
+
+log(chalk.magenta(`Messages will be translated in to the following locales: ${chalk.bold(locales.join(', '))} \n`));
+
+log(
+  chalk.magenta(
+    `Using OpenAPI key ******${chalk.bold(
+      key.slice(-NUMBER_OF_API_KEY_CHARACTERS_TO_SHOW),
+    )}`,
+  ),
+);
