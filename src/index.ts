f126031cfb7e1e334d5028f9ed6cf3601816ba49
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 16:20:44 +0100

    Remove module package type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import { program } from 'commander';
+
 console.log('hello world');
+
+program
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
+
+program.option('-k, --key <type>').option('-i, --input-path <type>');
+// .option('-l, --locales <type>', 'hm', commaSeparatedList);
