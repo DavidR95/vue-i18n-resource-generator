f1a6ce2a5bbabe9db597ce53a62618ba65e25a19
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 22:38:54 +0100

    Log OpenAI response

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,65 @@
 import chalk from 'chalk';
 import { command } from './command';
 import { readInput } from './io';
-import { MAX_TOKENS, initializeClient } from './api';
+import { MAX_TOKENS, initializeClient, sendRequest } from './api';
 import { createPrompt } from './prompt';
 
 const NUMBER_OF_API_KEY_CHARACTERS_TO_SHOW = 4;
 
 const { log } = console;
 
-const { key, inputPath, locales } = command.opts();
+const main = async (): Promise<void> => {
+  const { key, inputPath, locales } = command.opts();
 
-log(
-  chalk.magenta(
-    `Reading input messages located at: ${chalk.bold.underline(inputPath)}.`,
-  ),
-);
+  log(
+    chalk.magenta(
+      `Reading input messages located at: ${chalk.bold.underline(inputPath)}.`,
+    ),
+  );
 
-log(
-  chalk.red.italic(
-    'Note, it is assumed that this is a JSON file representing messages written in valid Vue I18n message syntax.\n',
-  ),
-);
+  log(
+    chalk.red.italic(
+      'Note, it is assumed that this is a JSON file representing messages written in valid Vue I18n message syntax.\n',
+    ),
+  );
 
-const inputMessages = readInput(inputPath);
+  const inputMessages = readInput(inputPath);
 
-log(
-  chalk.magenta(
-    `Found ${Object.keys(inputMessages).length} messages to translate.\n`,
-  ),
-);
+  log(
+    chalk.magenta(
+      `Found ${Object.keys(inputMessages).length} messages to translate.\n`,
+    ),
+  );
 
-log(
-  chalk.magenta(
-    `Initializing OpenAI API client using key ******${chalk.bold(
-      key.slice(-NUMBER_OF_API_KEY_CHARACTERS_TO_SHOW),
-    )} \n`,
-  ),
-);
+  log(
+    chalk.magenta(
+      `Initializing OpenAI API client using key ******${chalk.bold(
+        key.slice(-NUMBER_OF_API_KEY_CHARACTERS_TO_SHOW),
+      )} \n`,
+    ),
+  );
 
-initializeClient(key);
+  initializeClient(key);
 
-const prompt = createPrompt(inputMessages, locales, MAX_TOKENS / 2);
+  const prompt = createPrompt(inputMessages, locales, MAX_TOKENS / 2);
 
-log(
-  chalk.magenta(
-    `Requesting translations from OpenAI using the following prompt:\n`,
-  ),
-);
+  log(
+    chalk.magenta(
+      `Requesting translations from OpenAI using the following prompt:\n`,
+    ),
+  );
 
-log(`${chalk.blue.italic(prompt)}\n`);
+  log(`${chalk.blue.italic(prompt)}\n`);
 
-log(
-  chalk.magenta(
-    `Messages will be translated in to the following locales: ${chalk.bold(
-      locales.join(', '),
-    )} \n`,
-  ),
-);
+  const promptResponse = await sendRequest(prompt);
+
+  log(
+    chalk.magenta(
+      `OpenAI provided the following response to the previous prompt: ${chalk.green.italic(
+        promptResponse,
+      )}\n`,
+    ),
+  );
+}
+
+void (async () => main())();
