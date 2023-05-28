596419be3515d0f381cfe8835e55dcaa03acad99
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-28 17:03:51 +0100

    Improve help text formatting

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
-import { Command } from 'commander';
+import { createCommand } from 'commander';
 import { name, description, version } from '../package.json';
 
-const program = new Command();
+const command = createCommand();
 
-program.name(name).description(description).version(version)
+command
+  .name(name)
+  .description(description)
+  .version(version, '-v --version', 'Outputs the current library version.')
+  .helpOption('-h, --help', 'Displays help information.')
+  .showHelpAfterError('(add --help for additional information)');
 
-program
-  .requiredOption('-k, --key <key>', 'your OpenAI API key')
+command
+  .requiredOption('-k, --key <key>', 'Your OpenAI API key.')
   .requiredOption(
     '-i, --input-path <path>',
-    'the path to the JSON file containing the Vue I18n messages you would like to translate',
+    'The path to the JSON file containing the Vue I18n messages you would like to translate.',
   )
   .option(
     '-o, --output-path <path>',
-    'the path to the directory where you would like your translated JSON files to be written to',
-    process.cwd()
+    'The path to the directory where you would like your translated JSON files to be written to.',
+    process.cwd(),
   )
-  .requiredOption('-l, --locales <locales...>', 'the list of locales you would like to translate your input messages in to');
+  .requiredOption(
+    '-l, --locales <locales...>',
+    'The list of locales you would like to translate your input messages in to.',
+  );
 
-program.parse();
+command.parse();
 
-console.log(program.opts());
+console.log(command.opts());
