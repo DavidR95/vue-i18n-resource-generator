10f67965f974e75c677e3332700e6840ea0c3e07
DavidR95 <dmichaelrobertson@gmail.com>
2023-05-31 22:23:04 +0100

    Start simplifying main entry method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 import { command } from './command';
-import { readInput, writeLocaleMappedMessages } from './io';
+import { readInput, writeOutput } from './io';
 import {
   PROMPT_MAXIMUM_TOKENS,
   initializeClient,
@@ -10,6 +10,7 @@ import { createMultiLocalePrompt, createSingeLocalePrompt } from './prompt';
 import { convertCompletionToLocaleMappedMessages } from './completion';
 import { LocaleMappedMessages, Messages } from './messages';
 import partialJSONParse from 'partial-json-parser';
+import { CreateCompletionResponseChoicesInner } from 'openai';
 
 const main = async (): Promise<void> => {
   const { key, inputPath, outputPath, locales } = command.opts();
@@ -45,51 +46,67 @@ const main = async (): Promise<void> => {
 
   if (completion.finish_reason === 'stop') {
     handleStopFinishReason(localeMappedMessages, outputPath);
-
-    return;
+  } else {
+    await handleLengthFinishReason(
+      inputMessages,
+      locales,
+      completion,
+      localeMappedMessages,
+      outputPath,
+    );
   }
+};
 
-  const missingMessagesPerLocale: Record<string, string[]> = {};
-  const translatedMessagesPerLocale: LocaleMappedMessages = {};
+const handleStopFinishReason = (
+  localeMappedMessages: LocaleMappedMessages,
+  outputPath: string,
+): void => {
+  console.log(
+    chalk.magenta(
+      `OpenAI provided a finish reason of 'stop'. Now writing output JSON files to ${outputPath}.`,
+    ),
+  );
 
+  for (const [locale, messages] of Object.entries(localeMappedMessages)) {
+    writeOutput(outputPath, locale, messages);
+  }
+};
+
+const handleLengthFinishReason = async (
+  inputMessages: Messages,
+  locales: string[],
+  completion: CreateCompletionResponseChoicesInner,
+  localeMappedMessages: LocaleMappedMessages,
+  outputPath: string,
+): Promise<void> => {
   for (const locale of locales) {
-    const messages = localeMappedMessages[locale] ?? {};
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    while (
-      (missingMessagesPerLocale[locale] ?? []).length !== 0 ||
-      Object.keys(translatedMessagesPerLocale[locale] ?? []).length === 0
-    ) {
-      for (const [messageKey, messageValue] of Object.entries(messages)) {
-        if (!translatedMessagesPerLocale[locale]) {
-          translatedMessagesPerLocale[locale] = {};
-        }
+    const messages: Messages = localeMappedMessages[locale] ?? {};
+    let missingMessages: string[] = [];
 
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        translatedMessagesPerLocale[locale]![messageKey] = messageValue;
-      }
+    const stillHaveMessagesToTranslate =
+      missingMessages.length > 0 || Object.keys(messages).length === 0;
 
+    while (stillHaveMessagesToTranslate) {
       console.log(
         chalk.magenta(
-          `For ${locale}, we so far we have the following translated messages ${JSON.stringify(
-            translatedMessagesPerLocale[locale] ?? [],
+          `For ${locale}, so far we have the following translated messages ${JSON.stringify(
+            messages,
           )}\n`,
         ),
       );
 
-      const messageKeys = Object.keys(
-        translatedMessagesPerLocale[locale] ?? [],
-      );
+      const messageKeys = Object.keys(messages);
 
       const missingMessageKeys = Object.keys(inputMessages).filter(
         (messageKey) => !messageKeys.includes(messageKey),
       );
 
-      missingMessagesPerLocale[locale] = missingMessageKeys;
+      missingMessages = missingMessageKeys;
 
       console.log(
         chalk.magenta(
           `For ${locale}, we are missing the following translations ${JSON.stringify(
-            missingMessagesPerLocale[locale],
+            missingMessages,
           )}\n`,
         ),
       );
@@ -97,7 +114,7 @@ const main = async (): Promise<void> => {
       const remainingInputMessages = Object.entries(
         inputMessages,
       ).reduce<Messages>((remainingInputMessages, [key, value]) => {
-        if (missingMessagesPerLocale[locale]?.includes(key)) {
+        if (missingMessages.includes(key)) {
           remainingInputMessages[key] = value;
         }
 
@@ -131,47 +148,29 @@ const main = async (): Promise<void> => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const newMessages = partialJSONParse<Messages>(newCompletion.text!);
 
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       for (const [messageKey, messageValue] of Object.entries(newMessages)) {
-        if (!translatedMessagesPerLocale[locale]) {
-          translatedMessagesPerLocale[locale] = {};
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        translatedMessagesPerLocale[locale]![messageKey] = messageValue;
+        messages[messageKey] = messageValue;
       }
 
       console.log(
         chalk.magenta(
           `For ${locale}, we so far we have the following translated messages ${JSON.stringify(
-            translatedMessagesPerLocale[locale] ?? [],
+            messages,
           )}\n`,
         ),
       );
 
-      const messageKeys2 = Object.keys(
-        translatedMessagesPerLocale[locale] ?? [],
-      );
+      const messageKeys2 = Object.keys(messages);
 
       const missingMessageKeys2 = Object.keys(inputMessages).filter(
         (messageKey) => !messageKeys2.includes(messageKey),
       );
 
-      missingMessagesPerLocale[locale] = missingMessageKeys2;
+      missingMessages = missingMessageKeys2;
     }
+
+    writeOutput(outputPath, locale, messages);
   }
-
-  writeLocaleMappedMessages(translatedMessagesPerLocale, outputPath);
 };
-
-const handleStopFinishReason = (localeMappedMessages: LocaleMappedMessages, outputPath: string): void => {
-  console.log(
-    chalk.magenta(
-      `OpenAI provided a finish reason of 'stop'. Now writing output JSON files to ${outputPath}.`,
-    ),
-  );
-
-  writeLocaleMappedMessages(localeMappedMessages, outputPath);
-}
 
 void (async () => main())();
